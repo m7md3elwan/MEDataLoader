236ec0573a0b17f590e8ae1af6cf9f60e62b6631
Mohamed Mostafa <eng.m.elwan@gmail.com>
2019-08-10 14:23:47 +0200

    Initial Commit

---
+++ MEDataLoader/MEDataLoader.h
@@ -0,0 +1,19 @@
+//
+//  MEDataLoader.h
+//  MEDataLoader
+//
+//  Created by Mohamed Mostafa on 8/10/19.
+//  Copyright © 2019 Elwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MEDataLoader.
+FOUNDATION_EXPORT double MEDataLoaderVersionNumber;
+
+//! Project version string for MEDataLoader.
+FOUNDATION_EXPORT const unsigned char MEDataLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MEDataLoader/PublicHeader.h>
+
+
